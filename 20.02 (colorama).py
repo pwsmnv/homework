a3289528fa235b757b65fdf35762bfeca642dc10
avvecpx <166748558+pwsmnv@users.noreply.github.com>
2026-02-20 17:18:46 +0100

    Add files via upload

---
+++ 20.02 (colorama).py
@@ -0,0 +1,11 @@
+import colorama
+import inspect
+
+print(type(colorama))
+print(dir(colorama))
+print(id(colorama))
+help(colorama)
+
+print(inspect.isclass(colorama))
+print(inspect.isfunction(colorama))
+print(inspect.ismodule(colorama))
